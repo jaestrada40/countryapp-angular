5f6af6e530189c56d8abf4ea627f5fdff911bdae
Javier Estrada <javiera.estradag@gmail.com>
2024-09-23 16:11:13 -0600

    Primer commit

---
+++ src/app/shared/pages/contact-pages/contact-pages.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-contact-pages',
+  templateUrl: './contact-pages.component.html',
+})
+export class ContactPagesComponent {
+
+}
